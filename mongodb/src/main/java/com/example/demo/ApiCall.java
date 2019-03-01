bb92aa49fb496d18cf75388f6f1a8658bacae281
Rameshwar.Gholap <Rameshwar.Gholap@CEBS-IT-135.CEBS-CORPORATE.COM>
2019-03-01 12:08:37 +0530

    New added

--- mongodb/src/main/java/com/example/demo/ApiCall.java
+++ mongodb/src/main/java/com/example/demo/ApiCall.java
@@ -1,19 +1,13 @@
 package com.example.demo;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * 
